25a431a5a9190229963ddc9c0506c0e45c2fc0db
Kevin Day <kday@library.tamu.edu>
2022-08-10 15:23:38 -0500

    Update karma.conf.js and related or affected files.
    - Make `karma.conf.js` consistent across the UI repositories.
    - Update the preprocessor to include `index.html` by using `app/**/*.html`.
    - Update `karma.conf.js` to use single quotes.
    - Rename directories as necessary to be consistent across the UI repositories.
    - Add missing files as necessary to be consistent across the UI repositories.
    - Remove stale or invalid files as necessary to be consistent across the UI repositories.

---
+++ tests/mock/service/mockWsApi.js
@@ -0,0 +1,83 @@
+angular.module("mock.wsApi", []).service("WsApi", function ($q) {
+  var service = mockService($q);
+  var mapping;
+  var fetchResponse;
+
+  service.mockFetchResponse = function (data) {
+    if (data === null || data === undefined) {
+      fetchResponse = undefined;
+    } else {
+      // using hasOwnProperty() to support special test cases (such as checks to see if payload is undefined).
+      fetchResponse = {
+        type: data.hasOwnProperty("type") ? data.type : null,
+        payload: data.hasOwnProperty("payload") ? data.payload : {},
+        messageStatus: data.hasOwnProperty("messageStatus") ? data.messageStatus : null,
+        httpStatus: data.hasOwnProperty("httpStatus") ? data.httpStatus : null,
+        valueType: data.hasOwnProperty("valueType") ? data.valueType : null
+      };
+    }
+  };
+
+  service.mockMapping = function (toMock) {
+    mapping = {};
+    for (var key in toMock) {
+      mapping[key] = toMock[key];
+    }
+  };
+
+  service.fetch = function (apiReq, parameters) {
+    var payload = {};
+
+    if (fetchResponse) {
+      switch (fetchResponse.type) {
+        case "message":
+          return messagePromise($q.defer(), fetchResponse.payload, fetchResponse.messageStatus, fetchResponse.httpStatus);
+        case "value":
+          return valuePromise($q.defer(), fetchResponse.payload, fetchResponse.valueType);
+        case "payload":
+          return payloadPromise($q.defer(), fetchResponse.payload, fetchResponse.messageStatus, fetchResponse.httpStatus);
+        case "data":
+          return dataPromise($q.defer(), fetchResponse.payload, fetchResponse.messageStatus, fetchResponse.httpStatus);
+        case "reject":
+          return rejectPromise($q.defer(), fetchResponse.payload, fetchResponse.messageStatus, fetchResponse.httpStatus);
+        case "failure":
+          return failurePromise($q.defer(), fetchResponse.payload, fetchResponse.messageStatus, fetchResponse.httpStatus);
+      }
+    } else {
+      switch (apiReq.method) {
+        case "credentials":
+          payload = dataUser1;
+          break;
+        case "page":
+          payload = dataDocumentPage1;
+          break;
+        case "all":
+          switch (apiReq.controller) {
+            case "user":
+              payload = dataUser1;
+              break;
+          }
+          break;
+        case "get":
+          switch (apiReq.controller) {
+            case "user":
+              payload = dataUser1;
+              break;
+          }
+          break;
+      }
+    }
+
+    return payloadPromise($q.defer(), payload);
+  };
+
+  service.getMapping = function () {
+    return mapping;
+  };
+
+  service.listen = function (apiReq) {
+    return payloadPromise($q.defer());
+  };
+
+  return service;
+});
---
+++ tests/unit/repos/abstractAppRepoTest.js
@@ -0,0 +1,54 @@
+describe("service: abstractAppRepo", function () {
+  var $q, $rootScope, $scope, WsApi, repo, mockedRepo;
+
+  var initializeVariables = function (settings) {
+    inject(function (_$q_, _$rootScope_, _WsApi_) {
+      $q = _$q_;
+      $rootScope = _$rootScope_;
+
+      MockedUser = new mockUser($q);
+
+      WsApi = _WsApi_;
+    });
+  };
+
+  var initializeRepo = function (settings) {
+    inject(function ($injector) {
+      $scope = $rootScope.$new();
+      mockedRepo = new mockRepo("AbstractAppRepo", $q);
+      repo = $injector.get("AbstractAppRepo")();
+
+      // @fixme find a way to get something like `angular.extend(new mockRepo("AbstractAppRepo", $q), $injector.get("AbstractAppRepo")())` or `repo = AbstractAppRepo` to work.
+      repo.getAll = mockedRepo.getAll;
+      repo.listen = mockedRepo.listen;
+      repo.ready = mockedRepo.ready;
+    });
+  };
+
+  beforeEach(function () {
+    module("core");
+    module("app");
+    module("mock.user", function ($provide) {
+      var User = function () {
+        return MockedUser;
+      };
+      $provide.value("User", User);
+    });
+    module("mock.userService");
+    module("mock.wsApi");
+    module("templates");
+
+    initializeVariables();
+    initializeRepo();
+  });
+
+  afterEach(function () {
+    $scope.$destroy();
+  });
+
+  describe("Is the repo", function () {
+    it("defined", function () {
+      expect(repo).toBeDefined();
+    });
+  });
+});
